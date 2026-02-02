6028c01bd0f4d86f6180dce2964b9fccb14def6b
Your Name <your-email@example.com>
2026-02-02 02:45:55 -0800

    fix line endings

--- frontend/accountant/src/pages/dashboard/Dashboard.jsx
+++ frontend/accountant/src/pages/dashboard/Dashboard.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className='dashboard'>
+        <div className="dashboard-container">
+            dashboard
+        </div>
+    </div>
   )
 }
 
