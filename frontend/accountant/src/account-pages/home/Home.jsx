0ae1bc76be414996474a8c8a1cec0b7951079d0b
Your Name <your-email@example.com>
2026-02-11 00:13:37 -0800

    fic line endings

--- frontend/accountant/src/account-pages/home/Home.jsx
+++ frontend/accountant/src/account-pages/home/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
     <div className='home'>
       <div className="home-container">
         <div className="upper-container">
-          <button onClick={() => setIsSidebarOpen(prev => !prev)}><i class="fa-solid fa-bars"></i></button>
+          <button className='menubar-dashboard' onClick={() => setIsSidebarOpen(prev => !prev)}><i class="fa-solid fa-bars"></i></button>
           <input title='search your item' type="search" placeholder='search' /><button><i class="fa-solid fa-magnifying-glass"></i></button>
 
         </div>
