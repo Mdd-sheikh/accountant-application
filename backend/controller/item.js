cd7e158cab0cfc62c5c96f2bd8bbe18dd6b69c4a
Your Name <your-email@example.com>
2026-03-11 03:46:19 -0700

    fix line endings

--- backend/controller/item.js
+++ backend/controller/item.js
@@ -25,3 +25,49 @@ export const createItem = async (req, res) => {
 
   }
 };
+
+
+export const getItems = async (req, res) => {
+  try {
+    const items = await Item.find({ userId: req.user._id }); // only logged-in user's items
+
+    res.status(200).json({
+      success: true,
+      items
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+}
+
+
+export const updateItem = async (req, res) => {  
+  try {
+    const item = await Item.findOneAndUpdate(
+      { _id: req.params.id, userId: req.user._id }, // only logged-in user's item
+      req.body,
+      { new: true }
+
+    );
+    if (!item) {
+      return res.status(404).json({
+        success: false,
+        message: "Item not found"
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "Item updated successfully",
+      item
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+};
+
--- backend/routes/item.js
+++ backend/routes/item.js
@@ -1,9 +1,13 @@
 import express from  'express'
 import auth from '../middleware/auth.js'
 import { createItem } from '../controller/item.js'
+import { getItems } from '../controller/item.js'
+import { updateItem } from '../controller/item.js'
+
 
 const itemrouter = express.Router()
 
 itemrouter.post("/create/item",auth,createItem);
-
+itemrouter.get("/get/item",auth,getItems);
+itemrouter.put("/update/item/:id",auth,updateItem);
 export default itemrouter;
