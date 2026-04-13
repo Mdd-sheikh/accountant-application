425a3772591c336805056bbbd6825b4f2634cb45
Your Name <your-email@example.com>
2026-04-12 21:35:24 -0700

    fix line endings

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import customerRouter from './routes/customerRouter.js'
 import itemrouter from './routes/item.js'
 import Gstrouter from './routes/gstfetchRoute.js'
 import SignatureRouter from './routes/signatureRouter.js'
+import fs from 'fs'
 
 const app = express()
 const port = (process.env.PORT || 5000)
@@ -16,6 +17,11 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
+// automatic create folder for store images
+
+if (!fs.existsSync("uploads")) {
+    fs.mkdirSync("uploads");
+}
 // api endpoint
 
 app.use("/api/user", router)
@@ -23,7 +29,7 @@ app.use("/api/customer", customerRouter);
 app.use("/api/items", itemrouter);
 app.use("/api/gst", Gstrouter);
 app.use("/api/customer", SignatureRouter);
-app.use("/uploads", express.static("uploads")); 
+app.use("/uploads", express.static("uploads"));
 
 // mongo db cnnection
 DbConeection()
